7c518288ef85c6920fb6c4d642e6c949202f0373
Milia Anifieieva <milia.menzatova@gmail.com>
2020-10-13 13:21:20 -0400

    Create a new file for Quiz

---
+++ SumBetween/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumBetween
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Result is: " + sumBetween(5, 10));
+           Console.WriteLine("Result is: " + sumBetween(34, 67));
+        }
+
+        static int sumBetween(int a, int b) 
+        {
+          int sum = 0;
+          for (int i = a; i <= b; i++)
+          {
+            sum = sum + i;  
+          }
+          return sum;
+        }
+    }
+}
