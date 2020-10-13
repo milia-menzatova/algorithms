dc5aaa117ff96efd4cf57f98f86c57d687a23e75
Milia Anifieieva <milia.menzatova@gmail.com>
2020-10-13 17:48:28 -0400

    Create a file  for Quiz

---
+++ VowelsRemoval/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VowelsRemoval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Result string is: " + removeVowels(userInput));
+        }
+
+        static string removeVowels(string input)
+        {
+            string result = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+                if (ch == 'a' || ch == 'o' || ch == 'e' || ch == 'i' || ch == 'y' || ch == 'u')
+                {
+                    continue;
+                }
+                else
+                {
+                    result = result + ch;
+                }
+            }
+            return result;
+
+        }
+    }
+}
